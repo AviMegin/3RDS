61f3a54c76e14931b67980a6446cc2905c7f45d2
AviMegin <82816950+AviMegin@users.noreply.github.com>
2022-01-18 22:03:02 +0100

    big function

--- Program/leaderboard/leaderboard.js
+++ Program/leaderboard/leaderboard.js
@@ -1,134 +1,137 @@
 //fetching the data
 
-async function Users() {
-    let response = await fetch('https://i430712.hera.fhict.nl/Program/leaderboard/api/post.php')
-    let users = await response.json()
-    localStorage.setItem("quentinTarantino", JSON.stringify(users));
-    return users
-}
+// async function Users() {
+//     let response = await fetch('https://i430712.hera.fhict.nl/Program/leaderboard/api/post.php')
+//     let users = await response.json()
+//     localStorage.setItem("quentinTarantino", JSON.stringify(users));
 
-var retrievedData = localStorage.getItem("quentinTarantino");
-var myArray = JSON.parse(retrievedData);
-// var myArray = ""
-
-
-// fetch("https://i430712.hera.fhict.nl/Program/leaderboard/api/post.php").then(
-//     function(u) { return u.json(); }
-// ).then(
-//     function(json) {
-//         data_function(json); //calling and passing json to another function data_function
-//     }
-// )
-
-// //another functions
-// function data_function(data) {
-//     buildTable(data)
 // }
 
-
-//adding new user
-var newId = 4
-    //json for new user
-var newUser = {
-    'name': null,
-    'id': newId,
-    'points': null
-}
-$('#add-test').on('click', function() {
-    $('.form-wrapper').removeClass('hidden')
-})
+// var retrievedData = localStorage.getItem("quentinTarantino");
+// var myArray = JSON.parse(retrievedData);
 
 
-$('#test-result').on('keyup', function() {
-    newUser.points = $(this).val()
-    console.log(newUser)
 
-})
+// var myArray = []
 
-$('#test-name').on('change', function() {
-    newUser.name = $(this).val()
-    console.log(newUser)
-})
 
-$('#create-test').on('click', function() {
-        if (newUser.name == null) {
-            alert('no input')
+fetch("https://i430712.hera.fhict.nl/Program/leaderboard/api/post.php").then(
+    function(u) { return u.json(); }
+).then(
+    function(json) {
+        data_function(json); //calling and passing json to another function data_function
+    }
+)
+
+//another functions
+function data_function(data) {
+    var myArray = data;
+
+
+
+    //adding new user
+    var newId = 4
+        //json for new user
+    var newUser = {
+        'name': null,
+        'id': newId,
+        'points': null
+    }
+    $('#add-test').on('click', function() {
+        $('.form-wrapper').removeClass('hidden')
+    })
+
+
+    $('#test-result').on('keyup', function() {
+        newUser.points = $(this).val()
+        console.log(newUser)
+
+    })
+
+    $('#test-name').on('change', function() {
+        newUser.name = $(this).val()
+        console.log(newUser)
+    })
+
+    $('#create-test').on('click', function() {
+            if (newUser.name == null) {
+                alert('no input')
+            } else {
+                myArray.push({
+                    newUser
+                });
+                console.log(newUser)
+                $('#test-name').val('')
+                $('#test-result').val('')
+                addUser(newUser)
+
+                $('.form-wrapper').addClass('hidden')
+            }
+        })
+        //le data
+        //    fetch("")
+        //        .then((response) => response.json())
+        //        .then(data => obj = data)
+        //        .then(() => console.log(obj))
+        //    var myArray = obj;
+
+
+    //order by desc or asc event click
+    $('th').on('click', function() {
+        var column = $(this).data('column')
+        var order = $(this).data('order')
+        var text = $(this).html()
+        text = text.substring(0, text.length - 1)
+
+        if (order == 'desc') {
+            $(this).data('order', "asc")
+            myArray = myArray.sort((a, b) => a[column] > b[column] ? 1 : -1)
+            text += '&#9660'
+
         } else {
-            myArray.push({
-                newUser
-            });
-            console.log(newUser)
-            $('#test-name').val('')
-            $('#test-result').val('')
-            addUser(newUser)
+            $(this).data('order', "desc")
+            myArray = myArray.sort((a, b) => a[column] < b[column] ? 1 : -1)
+            text += '&#9650'
 
-            $('.form-wrapper').addClass('hidden')
         }
+        $(this).html(text)
+        buildTable(myArray)
     })
-    //le data
-    //    fetch("")
-    //        .then((response) => response.json())
-    //        .then(data => obj = data)
-    //        .then(() => console.log(obj))
-    //    var myArray = obj;
 
+    //search bar
 
-//order by desc or asc event click
-$('th').on('click', function() {
-    var column = $(this).data('column')
-    var order = $(this).data('order')
-    var text = $(this).html()
-    text = text.substring(0, text.length - 1)
+    $('#search-input').on('keyup', function() {
+            var value = $(this).val()
+            console.log(value) //console logging everything in input
 
-    if (order == 'desc') {
-        $(this).data('order', "asc")
-        myArray = myArray.sort((a, b) => a[column] > b[column] ? 1 : -1)
-        text += '&#9660'
-
-    } else {
-        $(this).data('order', "desc")
-        myArray = myArray.sort((a, b) => a[column] < b[column] ? 1 : -1)
-        text += '&#9650'
-
-    }
-    $(this).html(text)
-    buildTable(myArray)
-})
-
-//search bar
-
-$('#search-input').on('keyup', function() {
-        var value = $(this).val()
-        console.log(value) //console logging everything in input
-
-        var data = searchTable(value, myArray)
+            var data = searchTable(value, myArray)
 
 
 
-        buildTable(data)
-    })
-    //search function
+            buildTable(data)
+        })
+        //search function
 
-function searchTable(value, data) {
-    var filterData = []
-    for (var i = 0; i < data.length; i++) {
-        value = value.toLowerCase() //so search doesnt filter on caps
-        var name = data[i].name.toLowerCase()
-        if (name.includes(value)) {
-            filterData.push(data[i])
+    function searchTable(value, data) {
+        var filterData = []
+        for (var i = 0; i < data.length; i++) {
+            value = value.toLowerCase() //so search doesnt filter on caps
+            var name = data[i].name.toLowerCase()
+            if (name.includes(value)) {
+                filterData.push(data[i])
+
+            }
 
         }
 
+        return filterData
     }
 
-    return filterData
-}
-
-//add single new user
-function addUser(newUser) {
-    var table = document.getElementById('myTable')
-    var counter = 21
-    var row = `<tr class="row-${counter}" id="row">
+    //add single new user
+    function addUser(newUser) {
+        var table = document.getElementById('myTable')
+        var counter = 21
+        var row = `<tr class="row-${counter}" id="row">
                            <td>${counter}</td>
                            <td>${newUser.name}</td>
                            <td>${newUser.points}</td>
@@ -138,20 +141,20 @@ function addUser(newUser) {
                                
                                </td>
                      </tr>`
-    table.innerHTML += row
-    counter++;
+        table.innerHTML += row
+        counter++;
 
-}
+    }
 
-buildTable(myArray)
+    buildTable(myArray)
 
-//build the entire table
-function buildTable(data) {
-    var table = document.getElementById('myTable')
-    var counter = 1;
-    table.innerHTML = ''
-    for (var i = 0; i < data.length; i++) {
-        var row = `<tr class="row-${counter}" id="row">
+    //build the entire table
+    function buildTable(data) {
+        var table = document.getElementById('myTable')
+        var counter = 1;
+        table.innerHTML = ''
+        for (var i = 0; i < data.length; i++) {
+            var row = `<tr class="row-${counter}" id="row">
                            <td>${counter}</td>
                            <td id="result-${counter}">${data[i].name}</td>
                            <td id="result-${counter}">${data[i].points}</td>
@@ -161,36 +164,37 @@ function buildTable(data) {
                                
                                </td>
                      </tr>`
-            //    
-        table.innerHTML += row
-        counter++;
+                //    
+            table.innerHTML += row
+            counter++;
 
 
+
+
+
+        }
 
 
 
     }
 
+    //edit
 
 
-}
+    function edit(number) {
+        var row = number
+        console.log(row);
+        var editBtn = $(`.row-${row}`)
+        editBtn.addClass('hidden')
 
-//edit
+    }
+    //delete row
+    function deleteTest(number) {
+        var row = number
 
-
-function edit(number) {
-    var row = number
-    console.log(row);
-    var editBtn = $(`.row-${row}`)
-    editBtn.addClass('hidden')
-
-}
-//delete row
-function deleteTest(number) {
-    var row = number
-
-    var deleteBtn = $(`.row-${row}`)
-        // cancelBtn.removeClass('hidden')
-    deleteBtn.addClass('hidden')
-    console.log(row);
+        var deleteBtn = $(`.row-${row}`)
+            // cancelBtn.removeClass('hidden')
+        deleteBtn.addClass('hidden')
+        console.log(row);
+    }
 }
